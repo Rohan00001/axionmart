c04d2cf6c95f24c84aa814c12117261f2dd3ec6d
Rohan Gope <rg2512@it.jgec.ac.in>
2024-07-07 00:38:02 +0530

    chore: Refactor getOrderById API route for better error handling and response consistency

--- src/app/api/order-by-id/route.ts
+++ src/app/api/order-by-id/route.ts
@@ -2,49 +2,37 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '@/lib/dbConnect';
 import OrderModel from '@/model/Order.model';
 
-export default async function getOrderById(req: NextApiRequest) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     await dbConnect();
 
     try {
         const { id } = req.query;
         if (typeof id !== 'string') {
-            return Response.json(
-                {
-                    success: false,
-                    message: 'Invalid order ID provided',
-                },
-                { status: 500 }
-            );
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid order ID provided',
+            });
         }
 
         const order = await OrderModel.findById(id);
 
         if (!order) {
-            return Response.json(
-                {
-                    success: false,
-                    message: 'Order not found',
-                },
-                { status: 404 }
-            );
+            return res.status(404).json({
+                success: false,
+                message: 'Order not found',
+            });
         }
 
-        return Response.json(
-            {
-                success: true,
-                message: 'Order fetched successfully',
-                order,
-            },
-            { status: 200 }
-        );
+        return res.status(200).json({
+            success: true,
+            message: 'Order fetched successfully',
+            order,
+        });
     } catch (error) {
         console.error('Error fetching order:', error);
-        return Response.json(
-            {
-                success: false,
-                message: 'Error fetching order',
-            },
-            { status: 500 }
-        );
+        return res.status(500).json({
+            success: false,
+            message: 'Error fetching order',
+        });
     }
 }
